b0ab95b98449ff2774baa20082d2906e91a6cf62
Vincent Guilpain <guilpain-vincent@indetail.co.jp>
2019-04-22 16:36:33 +0900

    Add test for registerContract

--- test/scalardlTest.js
+++ test/scalardlTest.js
@@ -1,16 +1,35 @@
 let {
   ClientService,
-} = require('../scalardl');
+} = require('../scalardl-web-client-sdk');
 let {
   IllegalArgumentError,
 } = require('../illegal_argument_error');
 var expect = require('chai').expect;
 var assert = require('chai').assert;
+var sinon = require('sinon');
+
+const {
+  CertificateRegistrationRequest,
+  ContractRegistrationRequest,
+  ContractExecutionRequest,
+  LedgerValidationRequest,
+  ContractsListingRequest,
+} = require('../scalar_pb.js');
+
+const clientProperties = {
+  'scalar.ledger.client.server_host': 'localhost',
+  'scalar.ledger.client.server_port': '80',
+  'scalar.ledger.client.private_key_pem': 'key',
+  'scalar.ledger.client.cert_pem': 'cert',
+  'scalar.ledger.client.cert_holder_id': 'hold',
+};
 
 describe('The constructor', () => {
   describe('should throw an error', () => {
     it('when the private key is missing', () => {
       const clientProperties = {
+        'scalar.ledger.client.server_host': 'localhost',
+        'scalar.ledger.client.server_port': '80',
         // "scalar.ledger.client.private_key_pem": "key",
         'scalar.ledger.client.cert_pem': 'cert',
         'scalar.ledger.client.cert_holder_id': 'hold',
@@ -21,6 +40,8 @@ describe('The constructor', () => {
     });
     it('when the certificate is missing', () => {
       const clientProperties = {
+        'scalar.ledger.client.server_host': 'localhost',
+        'scalar.ledger.client.server_port': '80',
         'scalar.ledger.client.private_key_pem': 'key',
         // 'scalar.ledger.client.cert_pem': 'cert',
         'scalar.ledger.client.cert_holder_id': 'hold',
@@ -31,6 +52,8 @@ describe('The constructor', () => {
     });
     it('when holder id is missing', () => {
       const clientProperties = {
+        'scalar.ledger.client.server_host': 'localhost',
+        'scalar.ledger.client.server_port': '80',
         'scalar.ledger.client.private_key_pem': 'key',
         'scalar.ledger.client.cert_pem': 'cert',
         // 'scalar.ledger.client.cert_holder_id': 'hold',
@@ -41,18 +64,32 @@ describe('The constructor', () => {
     });
   });
 });
-it('Register contract should throw an error when contractBytes is not a Uint8Array',
-    async () => {
-      const clientProperties = {
-        'scalar.ledger.client.private_key_pem': 'key',
-        'scalar.ledger.client.cert_pem': 'cert',
-        'scalar.ledger.client.cert_holder_id': 'hold',
-      };
+
+describe('RegisterContract', () => {
+  describe('should throw an error', () => {
+    it('when contractBytes is not a Uint8Array', async () => {
       let service = new ClientService(clientProperties);
-      service.client = 'toto';
       try {
         await service.registerContract('contract1', 'foo', 'wrongType');
       } catch (e) {
         assert.instanceOf(e, IllegalArgumentError);
       }
     });
+  });
+  describe('should register the user without a cert version', () => {
+    it('when the version is not set in the properties', async () => {
+      const request = new CertificateRegistrationRequest();
+      request.setCertHolderId(
+          clientProperties['scalar.ledger.client.cert_holder_id']);
+      request.setCertVersion();
+      request.setCertPem(
+          clientProperties['scalar.ledger.client.cert_pem']);
+      let service = new ClientService(clientProperties);
+
+      const clientMock = sinon.mock(service.client);
+      await clientMock.expects('registerCert').once().withArgs(request, {});
+      service.registerCertificate();
+      clientMock.verify();
+    });
+  });
+});
