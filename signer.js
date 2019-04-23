f2d289f837e55f3ab69b7f6e87d089e28298132c
Plenty Su <plenty.su@gmail.com>
2019-04-23 15:42:34 +0900

    Use jsrsasign util to replace self-defined functions

--- signer.js
+++ signer.js
@@ -12,41 +12,22 @@ class SignatureSigner {
   }
 
   /**
-   * This method turns a hex value into a Uint8Array
-   * @param {number} hex
+   * This method is used for signing the content
+   * @param {Uint8Array} content
    * @return {Uint8Array}
    */
-  hex2uint8array(hex) {
-    return new Uint8Array(hex.match(/.{1,2}/g)
-        .map((byte) => parseInt(byte, 16)));
-  }
+  sign(content) {
+    const b64utohex = jsrsasign.b64utohex;
+    const hextoArrayBuffer = jsrsasign.hextoArrayBuffer;
+    const arrayBuffertohex = jsrsasign.ArrayBuffertohex;
 
-  /**
-   * This method turns a uint8array into a hex value
-   * @param {Uint8Array} array
-   * @return {string}
-   */
-  uint8array2hex(array) {
-    const buffer = array.buffer;
-    return Array.prototype.map.call(
-        new Uint8Array(buffer),
-        (x) => ('00' + x.toString(16)).slice(-2)).join('');
-  }
-
-  /**
-   * This method is used for signing the certificate
-   * @param {Uint8Array} buffer
-   * @return {Uint8Array}
-   */
-  sign(buffer) {
-    // const hex = this.uint8array2hex(buffer);
-    const hex = this.uint8array2hex(buffer);
+    const hex = arrayBuffertohex(content.buffer);
     const ecdsa = new jsrsasign.KJUR.crypto.ECDSA();
     try {
       const base64 = this.pem.replace('-----BEGIN EC PRIVATE KEY-----', '').
           replace('-----END EC PRIVATE KEY-----', '').
           replace(/\n/g, '');
-      ecdsa.readPKCS5PrvKeyHex(this.base64toHex(base64));
+      ecdsa.readPKCS5PrvKeyHex(b64utohex(base64));
     } catch (err) {
       throw new Error(`Failed to load private key ${err}`);
     }
@@ -57,25 +38,11 @@ class SignatureSigner {
       });
       signature.init(ecdsa);
       signature.updateHex(hex);
-      return this.hex2uint8array(signature.sign());
+
+      return new Uint8Array(hextoArrayBuffer(signature.sign()));
     } catch (err) {
       throw new Error(`Failed to sign the request ${err}`);
     }
-  }
-
-  /**
-   * Converts base64 to hex
-   * @param {number} base64
-   * @return {string}
-   */
-  base64toHex(base64) {
-    const raw = atob(base64);
-    let hex = '';
-    for (let i = 0; i < raw.length; i++) {
-      const _hex = raw.charCodeAt(i).toString(16);
-      hex += (_hex.length == 2 ? _hex : '0' + _hex);
-    }
-    return hex;
   }
 }
 
