8a671caedd8978211c3fe93b3acb9261b7745fab
Plenty Su <plenty.su@gmail.com>
2019-04-24 12:01:39 +0900

    Don't need to use variables for hooked functions

--- signer.js
+++ signer.js
@@ -17,17 +17,13 @@ class SignatureSigner {
    * @return {Uint8Array}
    */
   sign(content) {
-    const b64utohex = jsrsasign.b64utohex;
-    const hextoArrayBuffer = jsrsasign.hextoArrayBuffer;
-    const arrayBuffertohex = jsrsasign.ArrayBuffertohex;
-
-    const hex = arrayBuffertohex(content.buffer);
+    const hex = jsrsasign.ArrayBuffertohex(content.buffer);
     const ecdsa = new jsrsasign.KJUR.crypto.ECDSA();
     try {
       const base64 = this.pem.replace('-----BEGIN EC PRIVATE KEY-----', '').
           replace('-----END EC PRIVATE KEY-----', '').
           replace(/\n/g, '');
-      ecdsa.readPKCS5PrvKeyHex(b64utohex(base64));
+      ecdsa.readPKCS5PrvKeyHex(jsrsasign.b64utohex(base64));
     } catch (err) {
       throw new Error(`Failed to load private key ${err}`);
     }
@@ -39,7 +35,7 @@ class SignatureSigner {
       signature.init(ecdsa);
       signature.updateHex(hex);
 
-      return new Uint8Array(hextoArrayBuffer(signature.sign()));
+      return new Uint8Array(jsrsasign.hextoArrayBuffer(signature.sign()));
     } catch (err) {
       throw new Error(`Failed to sign the request ${err}`);
     }
