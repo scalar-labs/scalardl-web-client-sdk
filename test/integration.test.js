a304a53f0d5ad046a7fe1d45f1a72c15073f0ffd
Plenty Su <plenty.su@gmail.com>
2020-06-04 16:17:56 +0900

    Add integration test for indexedDB

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,5 +1,5 @@
 describe('ClientService', () => {
-  const {ClientService} = require('../scalardl-web-client-sdk.js');
+  const {ClientService, StatusCode} = require('../scalardl-web-client-sdk.js');
   const properties = {
     'scalar.dl.client.server.host': '127.0.0.1',
     'scalar.dl.client.server.port': 80,
@@ -147,6 +147,54 @@ describe('ClientService', () => {
         const response = await clientService.validateLedger(mockedAssetId);
         assert.equal(response.getCode(), 200);
       });
+    });
+  });
+
+  describe('Integration test with indexedDB enabled', function() {
+    it('should be able to use prestored private key', async function() {
+      const {Keystore} = require('../lib/keystore');
+      const holderId = `foo${Date.now()}`;
+      const certVersion = 1;
+      const keyId = `${holderId}_${certVersion}`;
+      const keystore = new Keystore('scalar');
+      const {toCryptoKeyFrom, toPkcs8From} = require('../lib/keyutil');
+      const pem = '-----BEGIN EC PRIVATE KEY-----\n' +
+        'MHcCAQEEICcJGMEw3dyXUGFu/5a36HqY0ynZi9gLUfKgYWMYgr/IoAoGCCqGSM49\n' +
+        'AwEHoUQDQgAEBGuhqumyh7BVNqcNKAQQipDGooUpURve2dO66pQCgjtSfu7lJV20\n' +
+        'XYWdrgo0Y3eXEhvK0lsURO9N0nrPiQWT4A==\n' +
+        '-----END EC PRIVATE KEY-----\n';
+      const key = await toCryptoKeyFrom(toPkcs8From(pem));
+      await keystore.put(keyId, key);
+      const properties = {
+        'scalar.dl.client.server.host': '127.0.0.1',
+        'scalar.dl.client.server.port': 80,
+        'scalar.dl.client.server.privileged_port': 8080,
+        'scalar.dl.client.cert_holder_id': holderId,
+        'scalar.dl.client.cert_version': certVersion,
+        'scalar.dl.client.private_key_indexeddb_enabled': true,
+        'scalar.dl.client.cert_pem': '-----BEGIN CERTIFICATE-----\n' +
+          'MIICizCCAjKgAwIBAgIUMEUDTdWsQpftFkqs6bCd6U++4nEwCgYIKoZIzj0EAwIw\n' +
+          'bzELMAkGA1UEBhMCSlAxDjAMBgNVBAgTBVRva3lvMQ4wDAYDVQQHEwVUb2t5bzEf\n' +
+          'MB0GA1UEChMWU2FtcGxlIEludGVybWVkaWF0ZSBDQTEfMB0GA1UEAxMWU2FtcGxl\n' +
+          'IEludGVybWVkaWF0ZSBDQTAeFw0xODA5MTAwODA3MDBaFw0yMTA5MDkwODA3MDBa\n' +
+          'MEUxCzAJBgNVBAYTAkFVMRMwEQYDVQQIEwpTb21lLVN0YXRlMSEwHwYDVQQKExhJ\n' +
+          'bnRlcm5ldCBXaWRnaXRzIFB0eSBMdGQwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNC\n' +
+          'AAQEa6Gq6bKHsFU2pw0oBBCKkMaihSlRG97Z07rqlAKCO1J+7uUlXbRdhZ2uCjRj\n' +
+          'd5cSG8rSWxRE703Ses+JBZPgo4HVMIHSMA4GA1UdDwEB/wQEAwIFoDATBgNVHSUE\n' +
+          'DDAKBggrBgEFBQcDAjAMBgNVHRMBAf8EAjAAMB0GA1UdDgQWBBRDd2MS9Ndo68PJ\n' +
+          'y9K/RNY6syZW0zAfBgNVHSMEGDAWgBR+Y+v8yByDNp39G7trYrTfZ0UjJzAxBggr\n' +
+          'BgEFBQcBAQQlMCMwIQYIKwYBBQUHMAGGFWh0dHA6Ly9sb2NhbGhvc3Q6ODg4OTAq\n' +
+          'BgNVHR8EIzAhMB+gHaAbhhlodHRwOi8vbG9jYWxob3N0Ojg4ODgvY3JsMAoGCCqG\n' +
+          'SM49BAMCA0cAMEQCIC/Bo4oNU6yHFLJeme5ApxoNdyu3rWyiqWPxJmJAr9L0AiBl\n' +
+          'Gc/v+yh4dHIDhCrimajTQAYOG9n0kajULI70Gg7TNw==\n' +
+          '-----END CERTIFICATE-----\n',
+      };
+      const clientService = await new ClientService(properties);
+
+      await clientService.registerCertificate();
+      const response = await clientService.validateLedger('non_existing_asset');
+
+      assert.equal(StatusCode.ASSET_NOT_FOUND, response.getCode());
     });
   });
 });
