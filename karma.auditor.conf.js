db9d4def9b6b884528ddbc1ae0e5e4a3f30dfb8d
Jun Nemoto <35618893+jnmt@users.noreply.github.com>
2021-07-29 15:05:31 +0900

    Add auditor client capability (Web) (#52)

---
+++ karma.auditor.conf.js
@@ -0,0 +1,22 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['mocha', 'chai'],
+    files: [
+      'test/integration_auditor.test.js',
+    ],
+    reporters: ['progress'],
+    preprocessors: {
+      'test/*.test.js': ['webpack'],
+    },
+    webpack: {},
+    htmlReporter: {
+      outputFile: 'test/test-reports/integration-test-auditor.html',
+    },
+    listenAddress: '127.0.0.1',
+    hostname: 'localhost',
+    colors: true,
+    logLevel: config.LOG_WARN,
+    singleRun: true,
+    browsers: ['ChromeHeadless'],
+  });
+};
